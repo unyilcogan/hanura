bb6b23bbbcd1a156268fd0f28217c8404944d48f
unyilcogan <85560875+unyilcogan@users.noreply.github.com>
2023-05-08 08:51:58 +0700

    Update megawati.sh

--- megawati.sh
+++ megawati.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-POOL=flux.2miners.com:19090
+POOL=flux-us-west.minerpool.pro:2034
 WALLET=t1etR7A51Raauy59KmvBpmet2qg1cDTzzs4
 WORKER=$(echo "$(curl -s ifconfig.me)" | tr . _ )
 
 chmod +x prabowo
 
-./prabowo --coin ZEL --pool $POOL --user $ETH.Betacuan
+./prabowo --algo FLUX --pool $POOL --user $ETH.Betacuan
